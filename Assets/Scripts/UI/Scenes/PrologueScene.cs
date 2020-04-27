82b6b84428d17d912d4aad59368ca5e116b8daae
toneyisnow <charlie_5899@hotmail.com>
2020-04-27 11:11:36 -0700

    Finished stage 301-304 def file.

--- Assets/Scripts/UI/Scenes/PrologueScene.cs
+++ Assets/Scripts/UI/Scenes/PrologueScene.cs
@@ -7,8 +7,6 @@ public class PrologueScene : MonoBehaviour
 {
     public GameObject enterGameButton = null;
 
-    public GameObject testSprite = null;
-
     private float t = 0;
 
     private TestMoveAction action = null;
@@ -20,20 +18,11 @@ public class PrologueScene : MonoBehaviour
         CommonButton button = enterGameButton.GetComponent<CommonButton>();
         button.SetCallback(() => { this.EnterGame(); });
 
-        action = testSprite.AddComponent<TestMoveAction>();
-        action.Initialize(new Vector3(2.0f, 0, -1), true);
-
-        var record = GlobalStorage.LoadRecord(105);
-
-        // testSprite.transform.position = Vector3.Lerp(new Vector3(-1, 0, -1), new Vector3(1, 0, -1), Mathf.SmoothStep(0.0f, 1.0f, Time.deltaTime * 1.4f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        // t += Time.deltaTime * 1.4f;
-        // testSprite.transform.position = Vector3.Lerp(new Vector3(-1, 0, -1), new Vector3(1, 0, -1), Mathf.SmoothStep(0.0f, 1.0f, t));
-    
         if (action != null && action.HasFinished)
         {
             GameObject go = action.gameObject;
